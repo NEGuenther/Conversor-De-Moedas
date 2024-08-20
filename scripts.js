704ebe8c2a0df3461820c0ed1811db896194fa15
NEGuenther <n.guenther1301@gmail.com>
2024-08-20 18:36:04 -0300

    Adding conversion function with Euro

--- scripts.js
+++ scripts.js
@@ -1,27 +1,32 @@
-const convertButton = document.querySelector(".convert-button")
-
+const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
-    const inputCurrencyValue = document.querySelector(".input-currency").value
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
-    const currencyValueConverted = document.querySelector(".currency-value")
+    const inputCurrencyValue = document.querySelector(".input-currency").value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
+    const currencyValueConverted = document.querySelector(".currency-value");
 
+    const dolarToday = 5.20;
+    const euroToday = 6.20;
 
-    const dolarToday = 5.20
-
-    const convertedValue = inputCurrencyValue / dolarToday
+    if (currencySelect.value === "dolar") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(inputCurrencyValue / dolarToday);
+        console.log(inputCurrencyValue / dolarToday);
+    } else if (currencySelect.value === "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(inputCurrencyValue / euroToday);
+        console.log(inputCurrencyValue / euroToday);
+    }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL",
-    }).format(inputCurrencyValue)
-
-    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD"
-    }).format(convertedValue)
-
-    console.log(inputCurrencyValue)
+    }).format(inputCurrencyValue);
 }
 
-convertButton.addEventListener("click", convertValues)
+convertButton.addEventListener("click", convertValues);
